ebeca9d6e7dc5c3a92894dd78a9d572e266b69e5
rizwangul-hub <rizwangul-hub>
2026-05-16 12:12:19 +0500

    change in server

--- services/api.js
+++ services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:7000/api",
+  baseURL: (process.env.NEXT_PUBLIC_API_URL || "http://localhost:7000/api").endsWith("/")
+    ? process.env.NEXT_PUBLIC_API_URL || "http://localhost:7000/api"
+    : `${process.env.NEXT_PUBLIC_API_URL || "http://localhost:7000/api"}/`,
 });
 
 api.interceptors.request.use((config) => {
